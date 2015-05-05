193c1aba9999fd81af842a39f9dc6a860f364384
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 17:58:27 -0700

    Part 2 (in progress) - login working, user profiles in progress.

--- model.py
+++ model.py
@@ -28,8 +28,6 @@ class User(db.Model):
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
 
-
-
 class Movie(db.Model):
     """Movie information."""
 
@@ -51,9 +49,17 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer)
+
+    #Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings", order_by=rating_id))
+    #Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings", order_by=rating_id))
+
 
     def __repr__(self):
         """Provide helpful representation when printed."""
--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,52 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template('homepage.html')
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/user-homepage')
+def user_homepage():
+    return "test"
+    #return render_template('user.html')
+    #get ratings and movie titles from user object
+
+
+@app.route('/login')
+def login():
+    """Login form"""
+
+    return render_template('login_form.html')
+
+@app.route('/login-submission', methods=['POST'])
+def handle_login():
+    """Handles the login form and adds the user to the session."""
+
+    user = User.query.filter_by(email=request.form['username']).first()
+ 
+    if user and (user.password == request.form['password']):
+        session['username'] = user.email
+        flash("Login successful!")
+        return render_template('homepage.html')
+        #return redirect('/user-homepage', DEBUG_TB_INTERCEPT_REDIRECTS=False)
+    else:
+        flash("Invalid login.")
+        return render_template('login_form.html')
+
+@app.route('/log-out')
+def log_out():
+    """Logs user out and removes user from the session."""
+
+    del session['username']
+    flash("Logged out.")
+
+    return render_template('login_form.html')
+
 
 
 if __name__ == "__main__":
